ede69e316c3f37caf970b589be70bb7cd0631299
josepablobermudezm <53883209+josepablobermudezm@users.noreply.github.com>
2019-10-03 10:54:30 -0600

    Comienzo

---
+++ PacmanFX/src/pacmanfx/PacmanFX.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacmanfx;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import pacmanfx.util.FlowController;
+
+/**
+ *
+ * @author Jose Pablo Bermudez
+ */
+public class PacmanFX extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        FlowController.getInstance().InitializeFlow(primaryStage, null);
+        FlowController.getInstance().goMain();
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ PacmanFX/src/pacmanfx/controller/MenuController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacmanfx.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author Jose Pablo Bermudez
+ */
+public class MenuController extends Controller{
+
+    @FXML
+    private Label menu;
+
+     
+
+    @Override
+    public void initialize() {
+    }
+    
+}
