e778b49f396c294adb143d3f607356a5da653dae
JordiRodriguezC <ycorrales120@gmail.com>
2019-10-06 14:58:51 -0600

    update

---
+++ PacmanFX/src/pacmanfx/model/Arista.java
@@ -0,0 +1,153 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacmanfx.model;
+/*
+import java.util.List;
+import javafx.event.EventHandler;
+import javafx.geometry.Point2D;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+
+/**
+ *
+ * @author JORDI RODRIGUEZ
+ */
+/*
+public class Arista extends Line {
+
+    private Nodo origen;
+    private Nodo destino;
+    private Integer peso;
+    private Boolean accidente;
+    private Boolean reparacion;
+    private Integer pesoOriginal;
+
+    public Integer getPesoOriginal() {
+        return pesoOriginal;
+    }
+
+    public void setPesoOriginal(Integer pesoOriginal) {
+        this.pesoOriginal = pesoOriginal;
+    }
+
+    public Boolean getReparacion() {
+        return reparacion;
+    }
+
+    public void setReparacion(Boolean reparacion) {
+        this.reparacion = reparacion;
+    }
+
+    public Boolean getAccidente() {
+        return accidente;
+    }
+
+    public void setAccidente(Boolean accidente) {
+        this.accidente = accidente;
+    }
+
+    public EventHandler<MouseEvent> getClick() {
+        return click;
+    }
+
+    public void setClick(EventHandler<MouseEvent> click) {
+        this.click = click;
+    }
+
+    private CierreCosevi cierre;
+
+    public Nodo getOrigen() {
+        return origen;
+    }
+
+    public Arista() {
+        super();
+    }
+
+    public Arista(double startX, double startY, double endX, double endY) {
+        super(startX, startY, endX, endY);
+        this.setStrokeWidth(10.00);
+        this.setStroke(Color.TRANSPARENT);
+        this.setOnMouseClicked(click);
+        this.accidente = false;
+        this.reparacion = false;
+    }
+
+    public void setOrigen(Nodo origen) {
+        this.origen = origen;
+    }
+
+    public Nodo getDestino() {
+        return destino;
+    }
+
+    public void setDestino(Nodo destino) {
+        this.destino = destino;
+    }
+
+    public Integer getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Integer peso) {
+        this.peso = peso;
+    }
+
+    public void agregarNodos(List<Nodo> nodos) {
+
+        Point2D inicio = new Point2D(0, 0);
+        Point2D fin = new Point2D(0, 0);
+        Nodo aux = new Nodo();
+        Nodo aux2 = new Nodo();
+        for (Nodo nodo : nodos) {
+            if (nodo.getCenterX() == getStartX() && nodo.getCenterY() == getStartY()) {
+                setOrigen(nodo);
+                nodo.getAristasAdyacentes().add(this);
+                inicio = nodo.getPuntoMapa();
+                aux=nodo;
+            } else if (nodo.getCenterX() == getEndX() && nodo.getCenterY() == getEndY()) {
+                setDestino(nodo);
+                nodo.getAristasAdyacentes().add(this);
+                fin = nodo.getPuntoMapa();
+                aux2=nodo;
+            }
+        }
+        Double p = inicio.distance(fin);
+        this.peso = p.intValue();
+        this.pesoOriginal = this.peso;
+    }
+
+    EventHandler<MouseEvent> click = (MouseEvent event) -> {
+        if (agregarAccidente) {
+            Accidente accidente = new Accidente();
+
+            Point2D pMedio = this.destino.getPuntoMapa().midpoint(this.origen.getPuntoMapa());
+
+            accidente.setLayoutX(pMedio.getX() - accidente.getFitHeight() / 2);
+            accidente.setLayoutY(pMedio.getY() - accidente.getFitWidth() / 2);
+
+            anchorPane.getChildren().add(accidente);
+            agregarAccidente = false;
+            PantPrincipalController.accidentes.add(this);
+            PantPrincipalController.imagenesAccidentes.add(accidente);
+            this.accidente = true;
+            PantPrincipalController.rutaNueva = true;
+        } else if (agregarReparacion) {
+            cierre = new CierreCosevi();
+            Point2D pMedio = this.destino.getPuntoMapa().midpoint(this.origen.getPuntoMapa());
+            cierre.setLayoutX(pMedio.getX() - cierre.getFitHeight() / 2);
+            cierre.setLayoutY(pMedio.getY() - cierre.getFitWidth() / 2);
+            anchorPane.getChildren().add(cierre);
+            agregarReparacion = false;
+            PantPrincipalController.imagenesCierres.add(cierre);
+            PantPrincipalController.cierresCosevi.add(cierre);
+            PantPrincipalController.rutaNueva = true;
+            this.reparacion = true;
+
+        }
+    };
+}*/
---
+++ PacmanFX/src/pacmanfx/model/Dijkstra.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacmanfx.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author JORDI RODRIGUEZ
+ *//*
+public class Dijkstra {
+
+    Mapa grafo;
+    ArrayList <Nodo> listaNodosAdyacentes;
+    ArrayList <Arista> aux = new ArrayList <Arista>();
+
+    public Dijkstra(Mapa grafo) {
+        this.grafo = grafo;
+        listaNodosAdyacentes = new ArrayList <Nodo>();
+    }
+
+    public Dijkstra() {
+    }
+
+    public boolean isContenido(Nodo nodo) {
+        boolean retornado = false;
+        for (Nodo n : listaNodosAdyacentes) {
+            if (n == nodo) {
+                retornado = true;
+            }
+        }
+        return retornado;
+    }
+
+    private void llenarConAdyacentes(Nodo nodo) {
+        if (nodo != null) {
+            List<Arista> listaAux = nodo.getAristasAdyacentes();
+            if (listaAux != null) {
+                for (Arista enlace : listaAux) {
+                    Nodo aux2;
+                    if (nodo.equals(enlace.getOrigen())) {
+                        aux2 = enlace.getDestino();
+                    } else {
+                        aux2 = enlace.getOrigen();
+                    }
+
+                    if (!aux2.isMarca()&&!enlace.getAccidente()&&!enlace.getReparacion()) {
+
+                        if (isContenido(aux2)) {
+                            int longitud = nodo.getLongitudCamino() + enlace.getPeso();
+                            if (aux2.getLongitudCamino() > longitud) {
+                                aux2.setLongitudCamino(longitud);
+                                aux2.setNodoAntecesorDisjktra(nodo);
+                            }
+                        } else {
+                            aux2.setLongitudCamino(nodo.getLongitudCamino() + enlace.getPeso());
+                            aux2.setNodoAntecesorDisjktra(nodo);
+                            listaNodosAdyacentes.add(aux2);
+                        }
+
+                    }
+                }
+            }
+        }
+        
+    }
+
+    public Nodo buscarMenor() {
+        Nodo aux = new Nodo();
+        aux.setLongitudCamino(9999999);
+
+        for (Nodo nodo : listaNodosAdyacentes) {
+            if (nodo.getLongitudCamino() < aux.getLongitudCamino()) {
+                aux = nodo;
+            }
+        }
+
+        return aux;
+    }
+
+    public ArrayList ejecutar(Nodo nodoInicio) {
+        nodoInicio.setLongitudCamino(0);
+        if (nodoInicio != null) {
+            listaNodosAdyacentes = new ArrayList<>();
+            listaNodosAdyacentes.add(nodoInicio);
+            while (!listaNodosAdyacentes.isEmpty()) {
+                Nodo menor = buscarMenor();
+                menor.setMarca(true);
+                listaNodosAdyacentes.remove(menor);
+                llenarConAdyacentes(menor);
+            }
+        }
+        return aux;
+    }
+
+    public ArrayList<Arista> getAux() {
+        return aux;
+    }
+
+    private void rutaCorta(Nodo nodoFinal) {
+        aux.clear();
+        Nodo nAux = nodoFinal;
+        for (int i = 0; i < PantPrincipalController.accidentes.size(); i++) {
+            Arista accidente = PantPrincipalController.accidentes.get(i);
+            for (Arista arista : aux) {
+                if(arista.equals(accidente)){
+                    aux.remove(accidente);
+                }
+                
+            }
+        }
+        while (nAux.getNodoAntecesorDisjktra() != null) {
+            aux.add(grafo.getArista(nAux,
+                    nAux.getNodoAntecesorDisjktra()));
+            nAux = nAux.getNodoAntecesorDisjktra();
+        }
+
+    }
+
+    public void marcarRutaCorta(Nodo nodoFinal, Color color) {      
+        if (nodoFinal != null) {
+            rutaCorta(nodoFinal);
+            aux.stream().forEach((t) -> {
+                if (t != null) {
+                    t.setStroke(color);
+                    t.setStrokeWidth(5);
+                }
+            });
+        }
+    }
+}*/
