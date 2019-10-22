8f375d00f3d0fd016fa7e9baf8d3571795e10a75
cjosue13 <solomaj12@gmail.com>
2019-10-22 11:37:37 -0600

    update

--- PacmanFX/src/pacmanfx/controller/Nivel8Controller.java
+++ PacmanFX/src/pacmanfx/controller/Nivel8Controller.java
@@ -562,7 +562,7 @@ public class Nivel8Controller extends Controller implements Initializable {
                 Line linea = new Line(posx, posy, posx2, posy2);
                 linea.setStroke(Paint.valueOf("RED"));
                 linea.setStrokeWidth(3.00);
-                this.root.getChildren().add(linea);
+                //this.root.getChildren().add(linea);
                 arista.agregarNodos(nodos);
                 aristas.add(arista);
             }
